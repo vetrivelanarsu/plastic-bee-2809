fc1b6524e2f1c5afe2e5c00354789330b88f00cf
Somesh534 <taksalsomesh@gmail.com>
2023-02-24 22:14:47 +0530

    day4fw21_0614

---
+++ home.js
@@ -0,0 +1,43 @@
+const slider = document.querySelector('.slider');
+const sliderContainer = document.querySelector('.slider-container');
+const images = document.querySelectorAll('.slider-container img');
+const backwardButton = document.querySelector('.slider-button-backward');
+const forwardButton = document.querySelector('.slider-button-forward');
+
+let currentIndex = 0;
+
+function forward() {
+  currentIndex++;
+  if (currentIndex > images.length - 1) {
+    currentIndex = 0;
+  }
+  updateSlider();
+}
+
+function backward() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  updateSlider();
+}
+
+function updateSlider() {
+  const translateX = `translateX(-${currentIndex * (100 + 20)}%)`;
+  sliderContainer.style.transform = translateX;
+}
+
+slider.addEventListener('mouseenter', () => {
+  backwardButton.style.display = 'block';
+  forwardButton.style.display = 'block';
+});
+
+slider.addEventListener('mouseleave', () => {
+  backwardButton.style.display = 'none';
+  forwardButton.style.display = 'none';
+});
+
+forwardButton.addEventListener('click', forward);
+backwardButton.addEventListener('click', backward);
+
+updateSlider();
