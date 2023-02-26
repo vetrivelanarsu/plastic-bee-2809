5a1850447f56acc6839f4dc153e8433bfe6bb556
Somesh534 <taksalsomesh@gmail.com>
2023-02-26 18:03:09 +0530

    day5fw21_0614

--- home.js
+++ home.js
@@ -41,3 +41,49 @@ forwardButton.addEventListener('click', forward);
 backwardButton.addEventListener('click', backward);
 
 updateSlider();
+
+
+
+
+
+
+
+
+
+
+
+//////////////////sign in/signup functionality end////////////////////////////////////////
+
+function openLoginForm() {
+  document.getElementById("loginForm").style.display = "block";
+}
+
+function closeLoginForm() {
+  document.getElementById("loginForm").style.display = "none";
+}
+let SignIn = document.querySelector("#signin")
+let SignUp = document.querySelector("#done");
+
+
+SignIn.addEventListener("click",function(event){
+  event.preventDefault();
+  console.log("working")
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  alert("Sign Up Successful");
+  window.location.assign("./home.html")
+
+})
+
+
+
+let one = document.getElementById("carti");
+
+one.addEventListener("click",()=>{
+  window.location.href="cart.html"
+})
+
+let two = document.getElementById("book");
+two.addEventListener("click",()=>{
+  window.location.href="product.html"
+})
